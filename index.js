e8c4e5d41574031ab0aa4f2f3e4791b201829bd5
Jim Kang <jimkang@gmail.com>
2016-07-27 08:49:59 -0400

    Implemented.

--- index.js
+++ index.js
@@ -7,7 +7,30 @@ function scaleToFit(opts) {
     content = opts.content;
   }
 
-  
+  var viewWidth = view.right - view.left;
+  var viewHeight = view.top - view.bottom;
+  var contentWidth = content.right - content.left;
+  var contentHeight = content.top - content.bottom;
+
+  var offsetX = content.left - view.left;
+  var offsetY = content.bottom - view.bottom;
+
+  var viewToContentRatioX = viewWidth/contentWidth;
+  var viewToContentRatioY = viewHeight/contentHeight;
+
+  var relevantRatio = viewToContentRatioY;
+  if (viewToContentRatioX < viewToContentRatioY) {
+    relevantRatio =  viewToContentRatioX;
+  }
+
+  return {
+    a: relevantRatio,
+    b: 0,
+    c: 0,
+    d: relevantRatio,
+    e: -relevantRatio * offsetX,
+    f: -relevantRatio * offsetY
+  };
 }
 
 module.exports = scaleToFit;
