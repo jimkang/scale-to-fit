803ec5ac5d3c910c7f7a859ca20296da42796b82
Jim Kang <jimkang@gmail.com>
2016-07-28 09:19:23 -0400

    Lay out form fields and SVG board in workspace.

--- workspace-src.js
+++ workspace-src.js
@@ -1,3 +1,26 @@
 var editize = require('editize');
+var d3 = require('d3-selection');
 
-editize(document.getElementById('content-x'));
+d3.select('#form').selectAll('.number-field').each(editizeThis);
+
+function editizeThis() {
+  editize(this).onEndEdit(render);
+}
+
+function render() {
+  var opts = {
+      view: {
+        left: parseFloat(d3.select('#view-left').text()),
+        bottom: parseFloat(d3.select('#view-bottom').text()),
+        right: parseFloat(d3.select('#view-right').text()),
+        top: parseFloat(d3.select('#view-top').text())
+      },
+      content: {
+        left: parseFloat(d3.select('#content-left').text()),
+        bottom: parseFloat(d3.select('#content-bottom').text()),
+        right: parseFloat(d3.select('#content-right').text()),
+        top: parseFloat(d3.select('#content-top').text())
+      }
+    };
+    console.log(opts);
+}
